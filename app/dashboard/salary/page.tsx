f08f749909d3df50f865622d1950b090ff3c5035
Satriaji Darmawan <satrna0@gmail.com>
2024-06-23 20:37:05 +0700

    setup env

--- app/dashboard/salary/page.tsx
+++ app/dashboard/salary/page.tsx
@@ -1,8 +1,10 @@
 import { DataTable } from "./data-table";
 import { Main, columns } from "./columns";
 
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL || process.env.NEXT_PUBLIC_PROD_URL;
+
 async function getData() {
-    const res = await fetch("http://localhost:3000/api/salary", { cache: 'no-store' })
+    const res = await fetch(`${baseURL}/api/salary`, { cache: 'no-store' })
     if (!res.ok) {
         // This will activate the closest `error.js` Error Boundary
         throw new Error('Failed to fetch data')
