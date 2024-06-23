2ba912315d5f97cd7d79394b692784c557fc400e
Satriaji Darmawan <satrna0@gmail.com>
2024-06-23 22:31:45 +0700

    fix the api url

--- app/dashboard/attendance/page.tsx
+++ app/dashboard/attendance/page.tsx
@@ -1,7 +1,7 @@
 import { DataTable } from "./data-table";
 import { Main, columns } from "./columns";
 
-const baseURL = process.env.NEXT_PUBLIC_BASE_URL || process.env.NEXT_PUBLIC_PROD_URL;
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL
 
 async function getData() {
     const res = await fetch(`${baseURL}/api/attendance`, { cache: 'no-store' })
